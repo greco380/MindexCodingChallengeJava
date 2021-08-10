01913b787629413f2905cb4017b68342e425b5d4
Josh Greco <NightCode@localhost.localdomain>
2021-08-10 19:23:05 -0400

    built out the interface and the Reporting Structure which declares the new properties

--- src/main/java/com/mindex/challenge/data/ReportingStructure.java
+++ src/main/java/com/mindex/challenge/data/ReportingStructure.java
@@ -1,4 +1,30 @@
 package com.mindex.challenge.data;
 
 public enum ReportingStructure {
+    ;
+    /*
+     * Here are the new properties for the employee and the number of reports that employee has
+     */
+    private String employee;
+    private int NumberOfReports;
+
+    /*
+     * below are the getters and setters for the two properties of employee
+     * and NumberOfReports
+     */
+    public String getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(String employee) {
+        this.employee = employee;
+    }
+
+    public int getNumberOfReports() {
+        return NumberOfReports;
+    }
+
+    public void setNumberOfReports(int numberOfReports) {
+        NumberOfReports = numberOfReports;
+    }
 }
--- src/main/java/com/mindex/challenge/service/ReportingService.java
+++ src/main/java/com/mindex/challenge/service/ReportingService.java
@@ -1,4 +1,9 @@
 package com.mindex.challenge.service;
 
+import com.mindex.challenge.data.ReportingStructure;
+/*
+ * An interface that calles the report method
+ */
 public interface ReportingService {
+    ReportingStructure report(String id);
 }
